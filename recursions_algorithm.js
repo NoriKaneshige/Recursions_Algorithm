43905901a5a2ea94fe46d0a4bbaabd415aacb638
Nori Kaneshige <39219420+NoriKaneshige@users.noreply.github.com>
2020-04-22 23:59:09 -0400

    Add files via upload

---
+++ recursions_algorithm.js
@@ -0,0 +1,425 @@
+// Iterative Version
+function countDown(num){
+    for(var i = num; i > 0; i--){
+        console.log(i);
+    }
+    console.log("All done!")
+}
+
+
+// Recursive Version
+function countDown(num){
+    if(num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+countDown(3)
+// 3
+// 2
+// 1
+
+
+// another recursive example
+function sumRange(num){
+   if(num === 1) return 1; 
+   return num + sumRange(num-1);
+}
+
+console.log(sumRange(4)) // 10
+// insert fig_1
+
+// factorial_iterative way
+function factorial(num){
+    let total = 1;
+    for(let i = num; i > 1; i--){
+        total *= i
+    }
+    return total;
+}
+
+
+// factorial_recursive way
+function factorial(num){
+    if(num === 1) return 1;
+    return num * factorial(num-1);
+}
+
+console.log(factorial(3)) // 6
+// insert fig_2
+
+
+// helper_method_recursion
+// collectOddValues() below is not recursive and it calls an inner function, helper(), which is recuersive
+function collectOddValues(arr){
+    
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+        
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+}
+
+console.log(collectOddValues([1,2,3,4,5,6,7,8,9])) // [ 1, 3, 5, 7, 9 ]
+
+// insert fig_3
+
+
+// collect_odds_pure_recursion
+// pure recursive
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+console.log(collectOddValues([1,2,3,4,5])) // [1,3,5]
+                                 
+
+
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.
+// This function should mimic the functionality of Math.pow()  - do not worry about negative bases and exponents.
+
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+
+console.log(power(2,0)) // 1
+console.log(power(2,2)) // 4
+console.log(power(2,4)) // 16
+
+
+
+// Write a function factorial which accepts a number and returns the factorial of that number.
+// A factorial is the product of an integer and all the integers below it;
+// e.g., factorial four ( 4! ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+
+function factorial(num){
+  if (num < 0 ) return 0;
+   if(num <= 1) return 1;
+    return num * factorial(num-1);
+}
+
+console.log(factorial(1)) // 1
+console.log(factorial(2)) // 2
+console.log(factorial(4)) // 24
+console.log(factorial(7)) // 5040
+
+
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+console.log(productOfArray([1,2,3])) // 6
+console.log(productOfArray([1,2,3,10])) // 60
+
+
+
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function 
+
+function recursiveRange(x){
+   if (x === 0 ) return 0;
+   return x + recursiveRange(x-1);
+}
+
+console.log(recursiveRange(6)) // 21
+console.log(recursiveRange(10)) // 55 
+
+
+
+// Write a recursive function called fib which accepts a number and returns the nth number
+// in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers
+// 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(4))// 3
+console.log(fib(10)) // 55
+console.log(fib(28)) // 317811
+console.log(fib(35)) // 9227465
+
+
+
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+function reverse(str){
+    if(str.length <= 1) return str;
+    return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse('awesome')) // 'emosewa'
+console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
+
+// Write a recursive function called isPalindrome which returns true
+// if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
+isPalindrome('awesome') // false
+isPalindrome('foobar') // false
+isPalindrome('tacocat') // true
+isPalindrome('amanaplanacanalpanama') // true
+isPalindrome('amanaplanacanalpandemonium') // false
+
+
+
+// Write a recursive function called someRecursive which accepts an array and a callback. 
+// The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+
+
+function someRecursive(array, callback) {
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursive(array.slice(1),callback);
+}
+
+const isOdd = val => val % 2 !== 0;
+
+console.log(someRecursive([1,2,3,4], isOdd)) // true
+console.log(someRecursive([4,6,8,9], isOdd)) // true
+console.log(someRecursive([4,6,8], isOdd)) // false
+console.log(someRecursive([4,6,8], val => val > 10)) // false
+
+
+
+// Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+
+function flatten(oldArr){
+  var newArr = []
+    for(var i = 0; i < oldArr.length; i++){
+        if(Array.isArray(oldArr[i])){
+            newArr = newArr.concat(flatten(oldArr[i]))
+        } else {
+            newArr.push(oldArr[i])
+        }
+  } 
+  return newArr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+
+
+
+// Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+
+function capitalizeFirst (array) {
+    debugger
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+  res.push(string);
+  return res;
+}
+
+console.log(capitalizeFirst(['car','taco','banana'])) // ['Car','Taco','Banana']
+
+
+
+// Write a recursive function called nestedEvenSum. Return the sum of all even numbers in an object which may contain nested objects.
+
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
+
+
+
+// Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+
+function capitalizeWords (array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+ 
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)) // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+
+
+// Write a function called stringifyNumbers which takes in an object and finds all of the values
+// which are numbers and converts them to strings. Recursion would be a great way to solve this!
+
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+console.log(stringifyNumbers(obj))
+
+// {
+//     num: "1",
+//     test: [],
+//     data: {
+//         val: "4",
+//         info: {
+//             isRight: true,
+//             random: "66"
+//         }
+//     }
+// }
+
+
+
+// Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string
+
+
+// Helper Method Recursion Version
+function collectStrings(obj) {
+    var stringsArr = [];
+ 
+    function gatherStrings(o) {
+        for(var key in o) {
+            if(typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if(typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+ 
+    gatherStrings(obj);
+ 
+    return stringsArr;
+}
+
+const obj_1 = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(collectStrings(obj_1)) // ["foo", "bar", "baz"])
+
+// Pure Recursion Version
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
+
+
+console.log(collectStrings(obj_1)) // ["foo", "bar", "baz"])
+
+
+
+
